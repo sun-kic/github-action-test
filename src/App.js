e4b71e6ffc472adee1237dad3c8a6b367c2e8dab
Yi Sun <sun@kic.ac.jp>
2024-11-29 03:07:04 +0200

    change again

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from my pc</h1>
+        <h1>Hello test again</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
