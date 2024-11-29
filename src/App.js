93d9b25a5935a4935919a896a93fdbe9f97f74ff
Yi Sun <sun@kic.ac.jp>
2024-11-29 04:02:41 +0200

    back

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from my pc</h1>
-        <h1>Hello test again</h1>
-        <h1>Hello test again 2</h1>
-        <h1>Hello test again 4</h1>
+        <h1>Hello from docker</h1>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
